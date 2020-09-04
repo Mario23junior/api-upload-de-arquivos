183a165cc86186f7ad73f3f36c06055c3b896979
Mario_junior23 <mariojunior3251@gmail.com>
2020-09-04 04:48:22 -0300

    Iniciando projeto de upload de arquivos

---
+++ src/main/java/com/uploadArquivos/ApiUploadDeArquivosApplication.java
@@ -0,0 +1,13 @@
+package com.uploadArquivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUploadDeArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUploadDeArquivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uploadArquivos/ApiUploadDeArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadArquivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUploadDeArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
